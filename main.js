1437c4d698c9603f6b7d80e565db002b740e80bf
Saulo Costa <saulotarsobc@gmail.com>
2022-10-03 14:52:40 -0300

    :fire:

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createWindow() {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
         },
-        frame: false,
+        // frame: false,
         titleBarStyle: "customButtonsOnHover",
         transparent: true,
         alwaysOnTop: true,
